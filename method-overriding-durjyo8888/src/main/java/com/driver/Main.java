90645252b4145d44b9e7fda3c1a8e76290a9beaa
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-07-05 20:36:15 +0530

    Add files via upload
    git init

---
+++ method-overriding-durjyo8888/src/main/java/com/driver/Main.java
@@ -0,0 +1,22 @@
+package com.driver;
+
+public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[]args){
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+  
+}
